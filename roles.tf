cb01222c2ba6ccfb89bc13acb9598a5f5e98784d
Ellis <tim.ellis@affinitywater.co.uk>
2018-08-06 12:22:56 +0100

    Added remote admin role as a temp measure

--- roles.tf
+++ roles.tf
@@ -1,3 +1,33 @@
+# Admin Role
+
+resource "aws_iam_role" "admin_role" {
+  name                = "admin_role"
+  assume_role_policy  =  <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::739672810541:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "Bool": {
+          "aws:MultiFactorAuthPresent": "true"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "admin_role_policy_attach" {
+  role = "${aws_iam_role.admin_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 # Standard EC2 role to be managed by SSM
 
 resource "aws_iam_role" "ec2_ssm_role" {
@@ -6,7 +36,7 @@ resource "aws_iam_role" "ec2_ssm_role" {
   assume_role_policy = "${local.ec2_assume_role}"
 }
 
-resource "aws_iam_role_policy_attachment" "sto-readonly-role-policy-attach" {
+resource "aws_iam_role_policy_attachment" "ec2_ssm_role_policy_attach" {
   role = "${aws_iam_role.ec2_ssm_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
