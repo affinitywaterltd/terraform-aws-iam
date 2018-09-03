1c54b50a890423bd2eb8605536ca378b9e8bee44
Ellis <tim.ellis@affinitywater.co.uk>
2018-09-03 13:34:22 +0100

    Attempting to input AWS account ID

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+# Account identity
+data "aws_caller_identity" "current" {}
+
+# EC2 role policy
 data "aws_iam_policy_document" "ec2_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -8,6 +12,8 @@ data "aws_iam_policy_document" "ec2_assume_role_policy" {
     }
   }
 }
+
+# Local to save time
 
 locals {
   ec2_assume_role = "${data.aws_iam_policy_document.ec2_assume_role_policy.json}"
