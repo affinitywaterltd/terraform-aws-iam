14d882ed291f75c077648b4ce3b0da2c32867c2a
Ellis <tim.ellis@affinitywater.co.uk>
2018-09-05 11:11:12 +0100

    Copy and paste error - removed duplicate

--- variables.tf
+++ variables.tf
@@ -1,22 +1,2 @@
 # Account identity
 data "aws_caller_identity" "current" {}
-
-
-
-# EC2 role policy
-data "aws_iam_policy_document" "ec2_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-# Local to save time
-
-locals {
-  ec2_assume_role = "${data.aws_iam_policy_document.ec2_assume_role_policy.json}"
-}
