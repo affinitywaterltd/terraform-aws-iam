1b8666a25d7db88d0574b376a807717f5dfb48f2
Ellis <tim.ellis@affinitywater.co.uk>
2018-09-05 10:36:39 +0100

    split roles into people and services for simplicity

---
+++ system_roles.tf
@@ -0,0 +1,20 @@
+# Standard EC2 role for servers to be managed by SSM
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "SSM_Role"
+
+  assume_role_policy = "${local.ec2_assume_role}"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_role_policy_attach" {
+  role       = "${aws_iam_role.ec2_ssm_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm_role" {
+  name = "ssm_role"
+  role = "${aws_iam_role.ec2_ssm_role.name}"
+}
+
+# 
+
--- user_roles.tf
+++ user_roles.tf
@@ -25,6 +25,7 @@ data "aws_iam_policy_document" "SSO_trust" {
       type        = "AWS"
       identifiers = ["arn:aws:iam::739672810541:root"]
     }
+
     condition {
       test     = "Bool"
       variable = "aws:MultiFactorAuthPresent"
@@ -44,23 +45,3 @@ resource "aws_iam_role" "admin_role" {
   name               = "AWLAdminRole"
   assume_role_policy = "${data.aws_iam_policy_document.SSO_trust.json}"
 }
-
-# Standard EC2 role for servers to be managed by SSM
-
-resource "aws_iam_role" "ec2_ssm_role" {
-  name = "SSM_Role"
-
-  assume_role_policy = "${local.ec2_assume_role}"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_ssm_role_policy_attach" {
-  role       = "${aws_iam_role.ec2_ssm_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
-}
-
-resource "aws_iam_instance_profile" "ec2_ssm_role" {
-  name = "ssm_role"
-  role = "${aws_iam_role.ec2_ssm_role.name}"
-}
-
-# 
