76ad54bae0fb2fcbe92f24d8e848407f2573db1c
Ellis <tim.ellis@affinitywater.co.uk>
2018-10-08 11:41:19 +0100

    Created lambda reporting role

--- system_roles.tf
+++ system_roles.tf
@@ -32,6 +32,37 @@ resource "aws_iam_instance_profile" "ec2_ssm_role" {
   role = "${aws_iam_role.ec2_ssm_role.name}"
 }
 
-# 
+###############
+#Lambda
+###############
 
-#
+# Lambda role policy
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+# Lambda reporting role - read and notification
+
+resource "aws_iam_role" "lambda_reporting_role" {
+  name = "Lambda_Reporting"
+
+  assume_role_policy = "${data.aws_iam_policy_document.lambda_assume_role_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_readonly_policy_attach" {
+  role       = "${aws_iam_role.lambda_reporting_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ses_policy_attach" {
+  role       = "${aws_iam_role.lambda_reporting_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
+}
