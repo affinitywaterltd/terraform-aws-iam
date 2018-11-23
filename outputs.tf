3eee93173bf2d9dd0e8ff4ac2ba446b582d76762
Ellis <tim.ellis@affinitywater.co.uk>
2018-11-23 14:45:06 +0000

    Added lambda reporting output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ec2_ssm_role" {
   description = "Default role for EC2 instances to give SSM access"
   value       = "${aws_iam_instance_profile.ec2_ssm_role.name}"
 }
+
+output "lambda_reporting_role" {
+  description = "Default role for EC2 instances to give SSM access"
+  value = "${aws_iam_instance_profile.lambda_reporting_role.name}"
+} 
