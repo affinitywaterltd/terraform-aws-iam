5c81c00185249591175bd58e189ac575af4111b4
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2018-12-05 11:41:53 +0000

    Remove role

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,3 @@ output "lambda_report_role" {
   description = "Default role for Lambda Reporting"
   value       = "${aws_iam_role.lambda_reporting_role.arn}"
 } 
-
-
-output "invoke_lambda_role" {
-  description = "Default role for Invoking Lambda Functions"
-  value       = "${aws_iam_role.invoke_lambda_role.arn}"
-} 
