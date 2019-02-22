b775d8269ed9e2be423850d9d34598456ba4bc1b
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-02-22 13:58:12 +0000

    Up[date trust relationships

--- system_roles.tf
+++ system_roles.tf
@@ -2,28 +2,27 @@
 #EC2
 ##############
 
-# EC2 role policy
-data "aws_iam_policy_document" "ec2_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-    principals {
-      type        = "Service"
-      identifiers = ["ssm.amazonaws.com"]
-    }
-  }
-}
-
 # Standard EC2 role for servers to be managed by SSM
 
 resource "aws_iam_role" "ec2_ssm_role" {
   name = "SSM_Role"
 
-  assume_role_policy = "${data.aws_iam_policy_document.ec2_assume_role_policy.json}"
+  assume_role_policy = <<EOF
+ {
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+
+       "Action": "sts:AssumeRole",
+       "Principal": {
+         "Service": [ "ec2.amazonaws.com", "ssm.amazonaws.com" ]
+       },
+       "Effect": "Allow",
+       "Sid": ""
+     }
+   ]
+ }
+ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_ssm_role_policy_attach" {
