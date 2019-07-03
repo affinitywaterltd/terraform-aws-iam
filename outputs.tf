744ec0904ebb21df0610343cacfa8dab9d1df880
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-07-03 11:32:30 +0100

    Add IAM output

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "lambda_report_role" {
   value       = "${aws_iam_role.lambda_reporting_role.arn}"
 } 
 
+output "lambda_snapshot_cleanupt_role" {
+  description = "Default role for Lambda Reporting"
+  value       = "${aws_iam_role.lambda_snapshot_cleanup_role.arn}"
+} 
+
 output "ssm_service_role" {
   description = "SSM Service Linked Role"
   value       = "${aws_iam_service_linked_role.iam_service_linked_role_for_ssm.arn}"
