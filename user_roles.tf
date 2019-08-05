389bf11da60d0ff3d5c55c4d870e437074321737
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-08-05 14:03:04 +0100

    Corrected to AWSCodeBuildAdminAccess

--- user_roles.tf
+++ user_roles.tf
@@ -332,7 +332,7 @@ resource "aws_iam_role_policy_attachment" "dev_lambda_role_policy_attach" {
 
 resource "aws_iam_role_policy_attachment" "dev_codebuild_role_policy_attach" {
   role       = "${aws_iam_role.dev_ops_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "dev_codecommit_role_policy_attach" {
