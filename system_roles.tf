2677b5c3effb4dcf76453a303063dc9bc82dbfd0
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-11-08 15:23:35 +0000

    Add IAM role globally

--- system_roles.tf
+++ system_roles.tf
@@ -572,3 +572,79 @@ resource "aws_iam_policy" "lambda_cloudwatch_logs_expiration_policy" {
 EOF
 }
 
+
+# Cloudwatch Monitoring Access
+resource "aws_iam_role" "app_grafana_cloudwatch_read_role" {
+  name               = "app_grafana_cloudwatch_read_role"
+assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::986618351900:role/SSM_Role"
+            },
+            "Action": "sts:AssumeRole"
+        },
+        {
+          "Sid": "",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": [
+              "ec2.amazonaws.com",
+              "sts.amazonaws.com"
+            ]
+          },
+          "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
+}
+
+
+
+resource "aws_iam_policy" "app_grafana_policy" {
+  name = "app_grafana_policy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowReadingMetricsFromCloudWatch",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:DescribeAlarmsForMetric",
+                "cloudwatch:ListMetrics",
+                "cloudwatch:GetMetricStatistics",
+                "cloudwatch:GetMetricData"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowReadingTagsInstancesRegionsFromEC2",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeTags",
+                "ec2:DescribeInstances",
+                "ec2:DescribeRegions"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowReadingResourcesForTags",
+            "Effect" : "Allow",
+            "Action" : "tag:GetResources",
+            "Resource" : "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "app_grafana_policy_attachment" {
+  role       = "${aws_iam_role.app_grafana_cloudwatch_read_role.name}"
+  policy_arn = "${aws_iam_policy.app_grafana_policy.arn}"
+}
