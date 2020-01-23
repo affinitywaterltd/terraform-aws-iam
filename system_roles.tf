3bfabcfe3c6c25b7cedaabee8ce0acbe584a8c40
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-01-23 13:55:07 +0000

    Update role attachment

--- system_roles.tf
+++ system_roles.tf
@@ -674,5 +674,5 @@ resource "aws_iam_role" "rds_enhanced_monitoring_role" {
 
 resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring_role_attach" {
   role       = "${aws_iam_role.rds_enhanced_monitoring_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/job-function/AmazonRDSEnhancedMonitoringRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
 }
