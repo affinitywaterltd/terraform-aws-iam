7dbbee932cd6ecfc1bbe311bc95874564096230e
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2020-08-05 07:24:23 +0100

    Add assume role policy for CodeCommit

--- system_roles.tf
+++ system_roles.tf
@@ -919,3 +919,42 @@ resource "aws_iam_policy_attachment" "replication" {
   roles      = [aws_iam_role.s3_accesslogs_bucket_replication_role.name]
   policy_arn = aws_iam_policy.s3_accesslogs_bucket_replication_policy.arn
 }
+
+
+#
+# CodeCommit Cross Account Access Role
+#
+resource "aws_iam_role" "app_cicd_codecommit_access_role" {
+  name               = "app_cicd_codecommit_access_role"
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::633033879498:role/ssm_role_custom_cicds"
+            },
+            "Action": "sts:AssumeRole"
+        },
+        {
+          "Sid": "",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": [
+              "ec2.amazonaws.com",
+              "sts.amazonaws.com"
+            ]
+          },
+          "Action": "sts:AssumeRole"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "app_cicd_codecommit_policy_attachment" {
+  role       = aws_iam_role.app_cicd_codecommit_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitPowerUser"
+}
